2eb5ac72c07e0838ae907ea72b4c438d938a7184
Hayatu Sanusi <hamichtech@gmail.com>
2021-09-30 03:39:20 +0100

    task 3

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/usr/python3
+"""  This module defines a square """
+
+
+class Square:
+    """ Try and except to check TypeError and ValueError """
+    def __init__(self, size=0):
+        """Initializes a square with a given size"""
+        if not ininstance(size, int):
+            raise TypeError('size must be an integer')
+        else:
+            if size < 0:
+                raise ValueError('size must not be >= 0')
+            else:
+                self.__size - size
