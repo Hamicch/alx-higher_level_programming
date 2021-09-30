515618532947dcdac95179807f80ac0d219a5921
Hayatu Sanusi <hamichtech@gmail.com>
2021-09-30 03:23:21 +0100

    task 1 fix

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -5,6 +5,7 @@ This module defines a suqare based on task 0 (0-square.py)
 
 
 class Square:
-
+    """ Square class """
     def __init__(self, size):
+        """ Class initialization """
         self.__size = size
