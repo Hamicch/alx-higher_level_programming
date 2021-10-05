81eb9db6283ae7c655f7356d55c5465d5b99fc03
hamicch <hamichtech@gmail.com>
2021-10-05 04:05:15 +0100

    task fix

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,26 +1,42 @@
-#!/usr/bin/pyhton3
-"""
-This module defines a rectangle based on task 0 (0-rectangle)
+#!/usr/bin/python3
+"""A module for working with rectangles.
 """
 
 
 class Rectangle:
-    """A 2D Polygon with 4 perpendicular sides.
+    """Represents a 2D Polygon with 4 perpendicular sides.
     """
     def __init__(self, width=0, height=0):
+        """Initializes a Rectangle with a given width and height.
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+        """
         self.width = width
         self.height = height
 
     @property
     def width(self):
+        """Retrieves the width of this Rectangle.
+        Returns:
+            int: The width of this Rectangle.
+        """
         return self.__width
 
     @property
     def height(self):
+        """Retrieves the height of this Rectangle.
+        Returns:
+            int: The height of this Rectangle.
+        """
         return self.__height
 
     @width.setter
     def width(self, value):
+        """Updates the width of this Rectangle.
+        Args:
+            value (int): The new width of this Rectangle.
+        """
         if not isinstance(value, int):
             raise TypeError('width must be an integer')
         elif value < 0:
@@ -30,6 +46,10 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
+        """Updates the height of this Rectangle.
+        Args:
+            value (int): The new height of this Rectangle.
+        """
         if not isinstance(value, int):
             raise TypeError('height must be an integer')
         elif value < 0:
