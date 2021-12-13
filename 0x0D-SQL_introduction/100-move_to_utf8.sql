2853079ed821a6b1ae3776bacfcc5d050d052ebe
Hayatu Sanusi <hamichtech@gmail.com>
2021-12-13 05:09:18 +0100

    advanced task 1 fix

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -3,16 +3,12 @@
 -- convert database to utf-8
 ALTER DATABASE hbtn_0c_0
 CHARACTER SET utf8mb4
-COLLATE utfmb4_unicode_ci;
+COLLATE utf8mb4_unicode_ci;
 
 -- use the database
 USE hbtn_0c_0;
 
 -- convert first_table to utf-8
 ALTER TABLE first_table
-CHARACTER SET utf8mb4
-COLLATE utfmb4_unicode_ci;
-
--- changes name VARCHAR(256)
--- CHARACTER SET utf8mb4
--- COLLATE utf8mb4_unicode_ci
+CONVERT TO CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
