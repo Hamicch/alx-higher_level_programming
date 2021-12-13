a6323ada426ad0e9655b3a55f8fae9ef9a6ff0a3
Hayatu Sanusi <hamichtech@gmail.com>
2021-12-13 04:06:33 +0100

    task 15

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,5 @@
+-- script that lists the number of records with the same score in the table second_table of the database hbtn_0c_0 --
+
+SELECT score, COUNT(number)
+FROM second_table
+ORDER BY MAX(number);
