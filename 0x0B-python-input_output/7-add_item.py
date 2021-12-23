ef2ebc78728b17e1ffc03d3a36b4440e848e8a9a
Hamicch <sanusihayatu01@gmail.com>
2021-12-23 16:12:14 +0100

    task 7 fix

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-"""
-The script adds arguments from the main to a Python list.
-"""
+'''task 7 module'''
 
-import os
+
 import sys
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-filename = 'add_item.json'
-if os.path.exists(filename):
-    l = load_from_json_file(filename)
-    l.extend(sys.argv[1:])
-    save_to_json_file(l, filename)
-else:
-    save_to_json_file(sys.argv[1:], filename)
+arglist = list(sys.argv[1:])
+
+try:
+    old_data = load_from_json_file('add_item.json')
+except Exception:
+    old_data = []
+
+old_data.extend(arglist)
+save_to_json_file(old_data, 'add_item.json')
