444bb19acc2b19539c1f52d1608c812a603c107b
Hamicch <sanusihayatu01@gmail.com>
2021-12-23 08:46:16 +0100

    task 2 fix

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,13 +1,11 @@
 #!/usr/bin/node
-// prints a message depending of the number of arguments passed
 const process = require('process');
-let args = process.argv.slice(2);
+const args = process.argv.slice(2);
 
-if(args.length === 0) {
-	console.log('No Arguments')
+if (args.length === 0) {
+  console.log('No Arguments');
 } else if (args.length === 1) {
-	console.log('Argument found')
-}
-else {
-	console.log('Arguments found')
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
